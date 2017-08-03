4da279bc58b5a96747136dc3b913fd2fa98da317
BarryHan363136 <htbyanghang2@163.com>
2017-08-03 16:52:56 +0800

    springboot email confgi and test

---
+++ src/test/java/com/iris/study/springboot/service/MailServiceTest.java
@@ -0,0 +1,17 @@
+package com.iris.study.springboot.service;
+
+import com.iris.study.springboot.base.BaseTest;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MailServiceTest extends BaseTest {
+
+    @Autowired
+    private MailService mailService;
+
+    @Test
+    public void testSendHtmlEmail(){
+        mailService.sendHtmlMail("363136830@qq.com", "test send email", "HTB TEST EMAIL WITH SPRING BOOT");
+    }
+
+}
