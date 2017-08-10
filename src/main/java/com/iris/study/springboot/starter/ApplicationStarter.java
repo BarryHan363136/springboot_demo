b78cf433a19580231bf2959ee667c850a0125d9c
hants <htbyang2@163.com>
2017-08-10 22:45:21 +0800

    springboot integrate kafka

--- src/main/java/com/iris/study/springboot/starter/ApplicationStarter.java
+++ src/main/java/com/iris/study/springboot/starter/ApplicationStarter.java
@@ -6,10 +6,17 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import javax.servlet.Filter;
+import javax.servlet.MultipartConfigElement;
+
 
 /**
  * TaskApplication [spring boot] 主方法
@@ -43,6 +50,36 @@ public class ApplicationStarter {
         httpRequestFactory.setReadTimeout(httpConnectionReadTimeout);
 
         return new RestTemplate(httpRequestFactory);
+    }
+
+    // 用于处理编码问题
+    @Bean
+    public Filter characterEncodingFilter() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+
+    // 文件上传限制
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        // 设置文件大小限制 ,超出设置页面会抛出异常信息，
+        // 这样在文件上传的地方就需要进行异常信息的处理了;
+        factory.setMaxFileSize("2560KB"); // KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("5120KB");
+        // Sets the directory location where files will be stored.
+        // factory.setLocation("路径地址");
+        return factory.createMultipartConfig();
+    }
+
+    //文件下载
+    @Bean
+    public HttpMessageConverters restFileDownloadSupport() {
+        ByteArrayHttpMessageConverter arrayHttpMessageConverter = new ByteArrayHttpMessageConverter();
+        return new HttpMessageConverters(arrayHttpMessageConverter);
     }
 
 //    @Bean
---
+++ src/test/java/com/iris/study/springboot/utils/FunctionTest.java
@@ -0,0 +1,20 @@
+package com.iris.study.springboot.utils;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+/**
+ * Created by hantongshan on 2017/8/10.
+ */
+public class FunctionTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(FunctionTest.class);
+
+    @Test
+    public void testThreads(){
+    }
+
+}
